10573e3282a2ac7663e5ba698ecd111c214a1adf
Carolina Duarte <carolinaadwarte@gmail.com>
2023-03-27 12:48:24 +0100

    working on custom actions - text-to-speech

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -25,3 +25,35 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+# This file implements the text to speech functionality. gTTS is implemented - a Python library and CLI tool to
+# interface with Google Translate’s text-to-speech API. Writes spoken mp3 data to a file, a file-like object
+# (bytestring) for further audio manipulation, or stdout.
+
+from rasa_sdk import Action
+from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+from gtts import gTTS
+import os
+
+
+class ActionTextToSpeech(Action):
+    def name(self):
+        return "action_text_to_speech"
+
+    def run(self, dispatcher, tracker, domain):
+        # Get the text input from the user
+        text = tracker.latest_message['text']
+
+        # Generate the speech output using gTTS
+        speech = gTTS(text=text, lang='pt-pt')
+
+        # Save the speech output as an audio file
+        speech_file = 'speech.mp3'
+        speech.save(speech_file)
+
+        # Send the speech output back to the user
+        dispatcher.utter_audio_file(speech_file)
+
+        # Return an empty list to update the chatbot's state
+        return []
